cf76e0c82bc84a9ce815964dd7bd06d440b51322
aaqlzim <ayoub1234aqlzim@gmail.com>
2021-02-27 17:43:56 +0100

    progress

--- Allocator.hpp
+++ Allocator.hpp
@@ -1,12 +1,62 @@
 #ifndef ALLOCATOR_HPP
 # define ALLOCATOR_HPP
+
+# include <memory>
+# include <limits>
+
 namespace ft {
-	template<typename T>
+	template<class T>
 	class Allocator {
 		public:
+			// using value_type = T;
 			typedef T value_type;
-			
+			typedef T* pointer;
+			typedef const T* const_pointer;
+			typedef void* void_pointer;
+			typedef const void* const_void_pointer;
+			typedef size_t size_type;
+			typedef ptrdiff_t difference_type;
+			template<class U>
+			struct rebind {
+				typedef Allocator<U> other;
+			};
+			Allocator( void ) {}
+			template<class U>
+			Allocator( Allocator<U> const & rhs ) {
+
+			}
+			~Allocator( void ) {}
+
+			pointer allocate(size_type numObjs) {
+				mAllocs += numObjs;
+				return static_cast<pointer>(operator new(sizeof(T) * numObjs));
+			}
+			pointer allocate(size_type numObjs, const_pointer hint) {
+				return allocate(numObjs);
+			}
+			void	deallocate(pointer p, size_type numObjs) {
+				operator delete(p);
+			}
+			size_type max_size() const {
+				return std::numeric_limits<size_type>::max();
+			}
+			// template<class U, class... Args>
+			// void	construct(U *p, Args && ...args) {
+			// 	new(p) U(std::forward<Args>(args)...);
+			// }
+			// template<class U>
+			// void	destroy(U *p) {
+			// 	p->~U();
+			// }
+			size_type get_allocator() const {
+				return mAllocs;
+			}
+		private:
+			static size_type mAllocs;
 	};
 };
+
+template<class T>
+typename ft::Allocator<T>::size_type ft::Allocator<T>::mAllocs = 0;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,23 +2,46 @@
 // #include "List.hpp"
 #include <vector>
 #include <iostream>
+#include <memory>
+#include "Allocator.hpp"
 
-template<typename type>
-struct Allocator : std::allocator<type> {
-	// public:
-	// 	Allocator<type>( void ) {}
-	type * allocate(size_t size) {
-		std::cout << "Allocation request size => " << size << std::endl;
-		return new type[size];
-	}
-	void	deallocate(type * ptr, size_t size) {
-		std::cout << "Deallocate size => " << size << std::endl;
-		delete [] ptr;
-	}
-};
+// template<typename type>
+// struct Allocator : std::allocator<type> {
+// 	// public:
+// 	// 	Allocator<type>( void ) {}
+// 	type * allocate(size_t size) {
+// 		std::cout << "Allocation request size => " << size << std::endl;
+// 		return new type[size];
+// 	}
+// 	void	deallocate(type * ptr, size_t size) {
+// 		std::cout << "Deallocate size => " << size << std::endl;
+// 		delete [] ptr;
+// 	}
+// };
+
+// namespace ft {
+// 	template<typename type>
+// 	class Allocator {
+// 		public:
+// 			typedef T value_type
+// 			Allocator( void );
+// 			~Allocator( void );
+// 	};
+// };
 
 int		main()
 {
+	std::vector<int , ft::Allocator<int> > v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	v.push_back(6);
+	// std::cout << v.get_allocator().get_allocator() << std::endl;
+	std::vector<int, ft::Allocator<int> >::iterator it;
+	for (it = v.begin(); it != v.end(); it++)
+		std::cout << *it << std::endl;
 	// ft::Node<int> *node = new ft::Node<int>(5);
 	// ft::Node<int> *node1 = new ft::Node<int>(6);
 	// ft::Node<int> *node2 = new ft::Node<int>(7);
@@ -35,14 +58,14 @@ int		main()
 	//ft::List<int> *l = new ft::List<int>(5);
 	//ft::List<int>::iterator it = l->begin();
 	//std::cout << *it << std::endl;
-	std::vector<int , Allocator<int> > v1;
-	v1.push_back(1);
-	v1.push_back(2);
-	v1.push_back(3);
-	v1.push_back(4);
-	for (auto & var: v1)
-		std::cout << var << std::endl;
+	// std::vector<int , Allocator<int> > v1;
+	// v1.push_back(1);
+	// v1.push_back(2);
+	// v1.push_back(3);
+	// v1.push_back(4);
+	// for (auto & var: v1)
+	// 	std::cout << var << std::endl;
 
-	std::getchar();
+	// std::getchar();
 	return 0;
 }

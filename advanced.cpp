164c6a3ae2fc61c431e67e7a6f4141ae2c4de691
pyt45 <hossamaqlzim107@gmail.com>
2021-11-08 21:14:09 +0100

    Advanced cpp

--- advanced.cpp
+++ advanced.cpp
@@ -1,50 +1,36 @@
-/*#include <iostream>
-#include <locale>
-#include <string>
-#include <codecvt>
-#include <fstream>
-
-int main ()
-{
-  std::u32string str ( U"\U00004f60\U0000597d" );  // ni hao (你好)
-
-  std::locale loc (std::locale(), new std::codecvt_utf8<char32_t>);
-  std::basic_ofstream<char32_t> ofs ("test.txt");
-  ofs.imbue(loc);
-
-  std::cout << "Writing to file (UTF-8)... ";
-  ofs << str;
-  std::cout << "done!\n";
-
-  return 0;
-}*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <future>
+#include <thread>
+#include <mutex>
 #include <cmath>
 #include <functional>
 #include <utility>
+#include <bitset>
+#include <string>
+
 #define __cpp_17 0
+
 class Test {
-    public:
-        std::string name;
-        int age;
-        Test(int a, std::string name): age(a), name(name) {}
+	public:
+		std::string name;
+		int age;
+		Test(int a, std::string name): age(a), name(name) {}
 };
+
 #if __cpp_17
-std::optional<std::string> createB(bool b) {
-    if (b)
-        return "Created";
-    return {};
-}
+	std::optional<std::string> createB(bool b) {
+		if (b)
+			return "Created";
+		return {};
+	}
 #else
     #define T 0
 #endif
 
 int main() {
-    /*Test a = Test(15, "jack");
+    Test a = Test(15, "jack");
     Test a1 = Test(18, "adb");
     std::vector<Test> v;
     v.push_back(a);
@@ -60,7 +46,7 @@ int main() {
     });
     std::for_each(v.begin(), v.end(), [](Test e) {
         std::cout << e.age << " " << e.name << std::endl;
-    });*/
+    });
     /*std::pointer_to_binary_function<double, double, double> powObj(pow);
     double num[] = {1.0, 2.0, 3.0, 4.0, 5.0};
     double squares[5];
@@ -68,14 +54,15 @@ int main() {
     for (auto i: squares)
         std::cout << i << " ";
     std::cout << std::endl;*/
-    //std::pair<int*, int*> p;
-    //int first[] = {1, 2, 3, 4, 5};
-    //int second[] = {1, 2, 8, 9, 10};
-    /*int rslt[5];
+    std::pair<int*, int*> p;
+    int first[] = {1, 2, 3, 4, 5};
+    int second[] = {1, 2, 8, 9, 10};
+    int rslt[5];
     std::transform(first, first + 5, second, rslt, std::plus<int>());
     for (auto i: rslt)
         std::cout << i << " ";
-    std::cout << std::endl;*/
-    //p = std::mismatch(first, first + 5, second, std::equal_to<int>());
-    //std::cout << "first mismatching pair is " << *p.first << " and " << *p.second << std::endl;
+    std::cout << std::endl;
+    p = std::mismatch(first, first + 5, second, std::equal_to<int>());
+    std::cout << "first mismatching pair is " << *p.first << " and " << *p.second << std::endl;
+	std::cout << "===================\n";
 }
